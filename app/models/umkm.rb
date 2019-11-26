46347068a033ec6d9b1e7682035ac1bf555f1831
miftahunajat <miftahunajat@gmail.com>
2019-11-26 18:49:12 +0700

    add fix error

--- app/models/umkm.rb
+++ app/models/umkm.rb
@@ -1,5 +1,5 @@
 class Umkm < ApplicationRecord
-  mount_uploader :image_url, ImagesUploader
+  mount_uploader :gambar, ImagesUploader
   mount_uploader :siup, ImagesUploader
   belongs_to :jenis_umkm
 end
