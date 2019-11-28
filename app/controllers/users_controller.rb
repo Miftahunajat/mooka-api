61175dae6c1a6280cf566ee3e737a77c6d15f7b6
miftahunajat <miftahunajat@gmail.com>
2019-11-28 10:33:19 +0700

    register user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render :show, status: :created, location: @user
+      render json: @user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
